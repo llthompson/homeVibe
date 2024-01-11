db18c812b719129a57ef6dec760c3856c55b2598
llthompson <lisalacythompson+github@gmail.com>
2024-01-10 18:03:48 -0600

    setup ngrok

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,32 @@
 //frontend/src/App.tsx
 
-import React, { useEffect, useState } from 'react';
 import './App.css';
-
 import Router from './Router'
 import { BrowserRouter } from 'react-router-dom'
 import Navigation from './components/Navigation'
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 function App() {
 
-
-
   return (
+    <Auth0Provider
+        domain="{yourDomain}"
+        clientId="{yourClientId}"
+        authorizationParams={{
+          redirect_uri: window.location.origin
+        }}
+    >
+    
     <ThemeProvider theme={theme}>
-
-
       <BrowserRouter>
         <Navigation />
         <Router />
       </BrowserRouter>
-
     </ThemeProvider>
 
+    </Auth0Provider>
   );
 }
 
